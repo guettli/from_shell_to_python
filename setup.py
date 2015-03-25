623f496fb5e45f6b36f89200fff546c6cec1ef99
Thomas Guettler <tguettler@tbz-pariv.de>
2015-03-25 13:42:26 +0100

    - added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='from_shell_to_python',
+    version='2015.1',
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+
+)
